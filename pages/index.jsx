24333c04f8c80b713b7694b8a344400073e3310e
franckniat <niatmanuel2002@gmail.com>
2024-02-18 21:46:39 +0100

    style  : add C# icon to stack section, edit style

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,12 +42,20 @@ export default function Home(){
               </HoverCard>
               
             </h2>
-            <h2 className='text-base md:text-lg mb-6'>Je suis étudiant et développeur web.</h2>
+            <h2 className='text-base md:text-lg mb-6'>Je suis <span className="text-red-600">étudiant</span> et <span className="text-red-600">développeur web</span>.</h2>
             <section className='flex items-center gap-8'>
-              <a href="https://github.com/franckniat" target='_blank' className='hover:text-gray-600'><Github className='w-5 h-5 md:w-7 md:h-7'/></a>
-              <a href="https://x.com/manuel_niat" target='_blank' className='hover:text-sky-600'><Twitter className='w-5 h-5 md:w-7 md:h-7'/></a>
-              <a href="https://www.linkedin.com/in/franck-niat/" target='_blank' className='hover:text-blue-600'><Linkedin className='w-5 h-5 md:w-7 md:h-7'/></a>
-              <a href="https://instagram.com/itsniatfranck" target='_blank' className='hover:text-red-400'><Instagram className='w-5 h-5 md:w-7 md:h-7'/></a>
+              <a href="https://github.com/franckniat" target='_blank' className='hover:text-gray-600'>
+                <Github className='w-5 h-5 md:w-7 md:h-7'/>
+              </a>
+              <a href="https://x.com/manuel_niat" target='_blank' className='hover:text-sky-600'>
+                <Twitter className='w-5 h-5 md:w-7 md:h-7'/>
+              </a>
+              <a href="https://www.linkedin.com/in/franck-niat/" target='_blank' className='hover:text-blue-600'>
+                <Linkedin className='w-5 h-5 md:w-7 md:h-7'/>
+              </a>
+              <a href="https://instagram.com/itsniatfranck" target='_blank' className='hover:text-red-400'>
+                <Instagram className='w-5 h-5 md:w-7 md:h-7'/>
+              </a>
             </section>
           </section>
           <section className="hidden md:block">
@@ -70,7 +78,8 @@ export default function Home(){
             <h3 className='text-justify'>
                 Je suis actuellement stagiaire chez FTEC un cabinet de la ville de
                  Douala qui offre des services de conseil, d{"'"}audit en informatique, 
-                 et de mise sur pied de solutions logicielles.
+                 et de mise sur pied de solutions logicielles. Mais je reste cependant ouvert
+                  à toutes opportunités de stage ou d{"'"}emploi dans le domaine informatique.
             </h3>
           </section>
 
@@ -202,6 +211,9 @@ export default function Home(){
                 height={500}
                 />
               </li>
+              <li className="w-10 h-10 flex items-center" title="C#">
+                <CSharpLogo/>
+              </li>
             </ul>
           </section>
 
@@ -228,5 +240,12 @@ export default function Home(){
         </section>
       </section>
     </section>
+  )
+}
+
+
+function CSharpLogo(){
+  return(
+    <svg height="288" preserveAspectRatio="xMidYMid" viewBox="0 0 256 288" width="256" xmlns="http://www.w3.org/2000/svg"><path d="m255.569 84.452376c-.002-4.83-1.035-9.098-3.124-12.761-2.052-3.602-5.125-6.621-9.247-9.008-34.025-19.619-68.083-39.178-102.097-58.81699995-9.17-5.294-18.061-5.101-27.163.269-13.543 7.98699995-81.348 46.83399995-101.553 58.53699995-8.321 4.817-12.37 12.189-12.372 21.771-.013 39.455 0 78.909-.013 118.365 0 4.724.991 8.909 2.988 12.517 2.053 3.711 5.169 6.813 9.386 9.254 20.206 11.703 88.02 50.547 101.56 58.536 9.106 5.373 17.997 5.565 27.17.269 34.015-19.64 68.075-39.198 102.105-58.817 4.217-2.44 7.333-5.544 9.386-9.252 1.994-3.608 2.987-7.793 2.987-12.518 0 0 0-78.889-.013-118.345" fill="#a179dc"/><path d="m128.182 143.241376-125.194 72.084c2.053 3.711 5.169 6.813 9.386 9.254 20.206 11.703 88.02 50.547 101.56 58.536 9.106 5.373 17.997 5.565 27.17.269 34.015-19.64 68.075-39.198 102.105-58.817 4.217-2.44 7.333-5.544 9.386-9.252z" fill="#280068"/><path d="m255.569 84.452376c-.002-4.83-1.035-9.098-3.124-12.761l-124.263 71.55 124.413 72.074c1.994-3.608 2.985-7.793 2.987-12.518 0 0 0-78.889-.013-118.345" fill="#390091"/><g fill="#fff"><path d="m201.892326 116.294008v13.473684h13.473684v-13.473684h6.736842v13.473684h13.473685v6.736842h-13.473685v13.473684h13.473685v6.736842h-13.473685v13.473684h-6.736842v-13.473684h-13.473684v13.473684h-6.736842v-13.473684h-13.473684v-6.736842h13.473684v-13.473684h-13.473684v-6.736842h13.473684v-13.473684zm13.473684 20.210526h-13.473684v13.473684h13.473684z"/><path d="m128.456752 48.625876c35.143771 0 65.827133 19.0862981 82.26181 47.4560675l-.16037-.2730675-41.348577 23.808283c-8.146656-13.793605-23.081479-23.1020873-40.213232-23.2937868l-.539631-.0030178c-26.125574 0-47.3060815 21.1793886-47.3060815 47.3049616 0 8.543615 2.2777748 16.552204 6.2389764 23.469476 8.1540981 14.235253 23.4829071 23.836606 41.0671051 23.836606 17.69277 0 33.108884-9.723357 41.221568-24.110835l-.197128.345313 41.286486 23.918037c-16.254398 28.129557-46.517408 47.156948-81.252701 47.536189l-1.058225.005774c-35.2545819 0-66.0252492-19.203824-82.4185122-47.72358-8.0029927-13.922969-12.5820476-30.064389-12.5820476-47.27698 0-52.4660524 42.5322682-94.99944 95.0005598-94.99944z"/></g></svg>
   )
 }
